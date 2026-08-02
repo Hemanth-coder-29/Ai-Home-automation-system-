9e75838f30c639747f46643be2314598b9c722b3
Hemanth Raghava <hemanthraghava2909@gmail.com>
2026-08-02 23:51:33 +0530

    Initial project setup and development environment

---
+++ test.py
@@ -0,0 +1,22 @@
+import cv2
+import mediapipe as mp
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import sklearn
+import serial
+import speech_recognition as sr
+import matplotlib.pyplot as plt
+
+print("=" * 50)
+print("All libraries imported successfully!")
+print("=" * 50)
+
+print("OpenCV Version:", cv2.__version__)
+print("MediaPipe Version:", mp.__version__)
+print("TensorFlow Version:", tf.__version__)
+print("NumPy Version:", np.__version__)
+print("Pandas Version:", pd.__version__)
+print("Scikit-Learn Version:", sklearn.__version__)
+print("PySerial Version:", serial.__version__)
+print("SpeechRecognition Version:", sr.__version__)
